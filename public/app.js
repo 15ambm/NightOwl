e85fef3c1fb65b56ef504e4ed14507f23d85e035
root <alastair.lewis10@gmail.com>
2020-02-01 19:54:31 -0500

    All main features are implemented, now working on location services

--- public/app.js
+++ public/app.js
@@ -1,45 +1,52 @@
-//import * as firebase from "@firebase/app";
-//import "@firebase/firestore";
-
+//Project ID: night-owl-50961
 
 document.addEventListener("DOMContentLoaded", event => {const app = firebase.app();});
 
+//Global user data for the session
 var user = "";
-var friends = []
+//Contains an array of email addresses of friends
+var friends = null;
 
-function googleLogin() {
+function startSession() {
+  //Logs into google and populates user data
   const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(result => {user = result.user;}).catch(console.log)
 
-  firebase.auth().signInWithPopup(provider).then(result => {
-  user = result.user;
-  //document.write("Welcome to Night Owl " + user.displayName);
-  console.log(JSON.stringify(result.user));
-  }).catch(console.log)
+  //Sets status of user to safe and drinks to 0 on session start
+  firebase.database().ref('/users/' + user.email.replace(/\./g, "_")).update({'status' : 'safe', 'drinks' : 0})
+
+  //Sets the friends variable to a list of friends emails
+  firebase.database().ref('/users/'+ user.email.replace(/\./g, "_") + "/friends").on('value', function(snapshot){
+    friends = Object.values(snapshot.val())
+    console.log(friends);
+  });
 }
 
+//Sets user's status to help
 function sendHelp() {
   console.log("Send Help to " + user.displayName);
   firebase.database().ref('/users/' + user.email.replace(/\./g, "_")).update({'status' : 'help'});
 }
 
+//Sets user's status to safe
 function cancelHelp() {
   console.log("Cancelling help to " + user.displayName);
   firebase.database().ref('/users/' + user.email.replace(/\./g, "_")).update({'status' : 'safe'});
 }
 
-function getFriends() {
-  firebase.database().ref('/users/'+ user.email.replace(/\./g, "_") + "/friends").on('value', function(snapshot){
-    console.log(snapshot.val());
-    friends = snapshot.val();
-  });
-}
-
+//Reads text from HTML text box and adds to friends list
 function addFriend(){
   var email = document.getElementById('friendEmail').value;
   console.log("Adding friend with email: " + email);
   firebase.database().ref('/users/'+user.email.replace(/\./g, "_") + "/friends").push(email.replace(/\./g, "_"));
 }
 
-function displayFriends() {
-  alert(friends.toString());
+//Displays status of all friends in real time
+function monitorFriends(){
+  firebase.database().ref('/users/'+ friends[0]+"/status").on('value', function(snapshot){
+    document.getElementById('ted').innerHTML = snapshot.val();
+  });
+  firebase.database().ref('/users/'+ friends[1]+"/status").on('value', function(snapshot){
+    document.getElementById('alex').innerHTML = snapshot.val();
+  });
 }
